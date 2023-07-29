11c7648bb664cecf192de304b49c55308e6e30b2
yandiyandhi <dev@bprbangunarta.co.id>
2023-07-30 00:58:26 +0700

    resolve

--- resources/views/master/user/index.blade.php
+++ resources/views/master/user/index.blade.php
@@ -20,7 +20,6 @@
                                             Data User
                                         </h2>
                                     </div>
-<<<<<<< HEAD
                                     <!-- Page title actions -->
                                     <div class="col-auto ms-auto d-print-none">
                                         <div class="btn-list">
@@ -35,135 +34,6 @@
                                                     <path d="M5 12l14 0"></path>
                                                 </svg>
                                                 Tambah
-=======
-                                    <h2 class="page-title">
-                                        Data User
-                                    </h2>
-                                </div>
-                                <!-- Page title actions -->
-                                <div class="col-auto ms-auto d-print-none">
-                                    <div class="btn-list">
-                                        <a href="#" class="btn btn-primary" data-bs-toggle="modal"
-                                            data-bs-target="#modal-tambah">
-                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                class="icon icon-tabler icon-tabler-plus" width="24" height="24"
-                                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                                stroke-linecap="round" stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                <path d="M12 5l0 14"></path>
-                                                <path d="M5 12l14 0"></path>
-                                            </svg>
-                                            Tambah
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card-body border-bottom py-3" style="margin-top:-7px;">
-
-                        <form action="{{ route('user.index') }}" method="GET">
-                            <div class="input-group mb-2">
-                                <input type="text" class="form-control" name="name" id="name"
-                                    placeholder="Nama Pengguna" value="{{ Request('name') }}">
-                                <button class="btn" type="submit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-filter"
-                                        width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
-                                        stroke="currentColor" fill="none" stroke-linecap="round"
-                                        stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <path
-                                            d="M4 4h16v2.172a2 2 0 0 1 -.586 1.414l-4.414 4.414v7l-6 2v-8.5l-4.48 -4.928a2 2 0 0 1 -.52 -1.345v-2.227z">
-                                        </path>
-                                    </svg>
-                                    Filter
-                                </button>
-                            </div>
-                        </form>
-
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-vcenter">
-                                <thead>
-                                    <tr>
-                                        <th class="text-center" width="3%">No</th>
-                                        <th class="text-center">Nama User</th>
-                                        <th class="text-center">Email</th>
-                                        <th class="text-center">Username</th>
-                                        <th class="text-center">Kode</th>
-                                        <th class="text-center">Kantor</th>
-                                        <th class="text-center">Hak Akses</th>
-                                        <th class="text-center">Status</th>
-                                        <th class="text-center" width="5%">Akses</th>
-                                        <th class="text-center" width="5%">Ubah</th>
-                                        <th class="text-center" width="5%">Hapus</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($users as $data)
-                                    <tr>
-                                        <td class="text-center">{{ $loop->iteration + $users->firstItem() -1}}</td>
-                                        <td>{{ $data->name }}</td>
-                                        <td>{{ $data->email }}</td>
-                                        <td>{{ $data->username }}</td>
-                                        <td class="text-center">{{ $data->code_user }}</td>
-                                        <td>{{ $data->nama_kantor }}</td>
-                                        <td>
-                                            @if (empty($data->position))
-                                            <font>-</font>
-                                            @else
-                                            {{ $data->position }}
-                                            @endif
-
-                                        </td>
-                                        <td class="text-center">
-                                            @if ($data->is_active == 1)
-                                            <span class="badge bg-success-lt">Aktif</span>
-                                            @else
-                                            <span class="badge bg-danger-lt">Tidak Aktif</span>
-                                            @endif
-
-                                        </td>
-                                        <td class="text-center">
-                                            <a href="#" class="edit" data-bs-toggle="modal"
-                                                data-bs-target="#modal-akses">
-                                                <span class="badge bg-success">
-                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                        class="icon icon-tabler icon-tabler-lock-access" width="24"
-                                                        height="24" viewBox="0 0 24 24" stroke-width="2"
-                                                        stroke="currentColor" fill="none" stroke-linecap="round"
-                                                        stroke-linejoin="round">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                        <path d="M4 8v-2a2 2 0 0 1 2 -2h2"></path>
-                                                        <path d="M4 16v2a2 2 0 0 0 2 2h2"></path>
-                                                        <path d="M16 4h2a2 2 0 0 1 2 2v2"></path>
-                                                        <path d="M16 20h2a2 2 0 0 0 2 -2v-2"></path>
-                                                        <path
-                                                            d="M8 11m0 1a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1v3a1 1 0 0 1 -1 1h-6a1 1 0 0 1 -1 -1z">
-                                                        </path>
-                                                        <path d="M10 11v-2a2 2 0 1 1 4 0v2"></path>
-                                                    </svg>
-                                                </span>
-                                            </a>
-                                        </td>
-                                        <td class="text-center">
-                                            <a href="#" class="edit">
-                                                <span class="badge bg-warning">
-                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                        class="icon icon-tabler icon-tabler-edit" width="24" height="24"
-                                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                                                        fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                        <path
-                                                            d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1">
-                                                        </path>
-                                                        <path
-                                                            d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z">
-                                                        </path>
-                                                        <path d="M16 5l3 3"></path>
-                                                    </svg>
-                                                </span>
->>>>>>> 8222b51f3c6b802fb9a7388fb0f4c70a33b64159
                                             </a>
                                         </div>
                                     </div>
@@ -225,7 +95,6 @@
                                                         {{ $data->position }}
                                                     @endif
 
-<<<<<<< HEAD
                                                 </td>
                                                 <td class="text-center">
                                                     @if ($data->is_active == 1)
@@ -289,28 +158,6 @@
 
                                 {{ $users->links('vendor.pagination.bootstrap-5') }}
 
-=======
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label">Kantor</label>
-                                <select class="form-control" name="kantor_kode" id="kantor_kode">
-                                    <option value="">--Pilih Kantor--</option>
-                                    @foreach ($kantor as $data)
-                                    <option value="{{ $data->kode_kantor }}">{{ $data->nama_kantor }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label">Status</label>
-                                <select class="form-control" name="is_active" id="is_active">
-                                    <option value="">--Pilih Status--</option>
-                                    <option value="1">Aktif</option>
-                                    <option value="0">Tidak Aktif</option>
-                                </select>
->>>>>>> 8222b51f3c6b802fb9a7388fb0f4c70a33b64159
                             </div>
                         </div>
                     </div>
@@ -319,7 +166,6 @@
             </div>
         </div>
     </div>
-<<<<<<< HEAD
 
 
     <div class="modal modal-blur fade" id="modal-tambah" tabindex="-1" role="dialog" aria-hidden="true">
@@ -406,66 +252,10 @@
 
                     <div class="modal-footer">
                         <a href="#" class="btn btn-link link-secondary" data-bs-dismiss="modal">Batal</a>
-                        <button type="submit" class="btn btn-primary text-white ms-auto" id="Submit"
-                            name="submit">Simpan</button>
+                        <a href="#" class="btn btn-primary ms-auto">Simpan</a>
                     </div>
                 </form>
             </div>
         </div>
     </div>
 @endsection
-=======
-</div>
-
-<div class="modal modal-blur fade" id="modal-akses" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-xs modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Hak Akses</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-
-            <form action="#" method="POST">
-                @csrf
-                <div class="modal-body">
-                    <div class="row">
-
-                        <div class="col-lg-12">
-                            <div class="mb-3">
-                                <label class="form-label">ID User</label>
-                                <input type="text" class="form-control" name="id" id="id" value="1" readonly>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-12">
-                            <div class="mb-3">
-                                <label class="form-label">Nama Lengkap</label>
-                                <input type="text" class="form-control" name="name" id="name" value="Zulfadli Rizal"
-                                    readonly>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-12">
-                            <div class="mb-3">
-                                <label class="form-label">Hak Akses</label>
-                                <select class="form-control" name="role_id" id="role_id">
-                                    <option value="">--Pilih Akses--</option>
-                                    @foreach ($roles as $data)
-                                    <option value="{{ $data->id }}">{{ $data->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="modal-footer">
-                    <a href="#" class="btn btn-link link-secondary" data-bs-dismiss="modal">Batal</a>
-                    <a href="#" class="btn btn-primary ms-auto">Simpan</a>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-@endsection
->>>>>>> 8222b51f3c6b802fb9a7388fb0f4c70a33b64159
