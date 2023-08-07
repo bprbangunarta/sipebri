305e6610da7084b0df598d774f12dd39cede6d9b
Zulfadli Rizal <dev@bprbangunarta.co.id>
2023-08-07 09:46:37 +0700

    fix name

--- resources/views/survei/edit.blade.php
+++ resources/views/survei/edit.blade.php
@@ -3,127 +3,127 @@
 
 @section('content')
 <div class="page-body">
-    <div class="container-xl">
-        <div class="row row-deck row-cards">
+	<div class="container-xl">
+		<div class="row row-deck row-cards">
 
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="container-xl">
-                            <div class="row g-2 align-items-center">
-                                <div class="col">
-                                    <!-- Page pre-title -->
-                                    <div class="page-pretitle">
-                                        Pendaftaran
-                                    </div>
-                                    <h2 class="page-title">
-                                        Data Survayor
-                                    </h2>
-                                </div>
-                                <!-- Page title actions -->
-                                <div class="col-auto ms-auto d-print-none">
-                                    <div class="btn-list">
-                                        <a href="{{ route('pendamping.edit') }}" class="btn btn-primary">
-                                            <svg xmlns="http://www.w3.org/2000/svg"
-                                                class="icon icon-tabler icon-tabler-arrow-left" width="24" height="24"
-                                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                                stroke-linecap="round" stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                <path d="M5 12l14 0"></path>
-                                                <path d="M5 12l6 6"></path>
-                                                <path d="M5 12l6 -6"></path>
-                                            </svg>
-                                            Kembali
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+			<div class="col-12">
+				<div class="card">
+					<div class="card-header">
+						<div class="container-xl">
+							<div class="row g-2 align-items-center">
+								<div class="col">
+									<!-- Page pre-title -->
+									<div class="page-pretitle">
+										Pendaftaran
+									</div>
+									<h2 class="page-title">
+										Data Survayor
+									</h2>
+								</div>
+								<!-- Page title actions -->
+								<div class="col-auto ms-auto d-print-none">
+									<div class="btn-list">
+										<a href="{{ route('pendamping.edit') }}" class="btn btn-primary">
+											<svg xmlns="http://www.w3.org/2000/svg"
+												class="icon icon-tabler icon-tabler-arrow-left" width="24" height="24"
+												viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+												stroke-linecap="round" stroke-linejoin="round">
+												<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+												<path d="M5 12l14 0"></path>
+												<path d="M5 12l6 6"></path>
+												<path d="M5 12l6 -6"></path>
+											</svg>
+											Kembali
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 
-                    <div class="card-body border-bottom py-3">
-                        <div class="card">
-                            <div class="row g-0">
-                                <div class="col-3 d-none d-md-block border-end">
-                                    <div class="card-body">
-                                        @include('templates.menu-pendaftaran')
-                                    </div>
-                                </div>
-                                <div class="col d-flex flex-column">
-                                    <form action="#">
-                                        <div class="card-body">
+					<div class="card-body border-bottom py-3">
+						<div class="card">
+							<div class="row g-0">
+								<div class="col-3 d-none d-md-block border-end">
+									<div class="card-body">
+										@include('templates.menu-pendaftaran')
+									</div>
+								</div>
+								<div class="col d-flex flex-column">
+									<form action="#">
+										<div class="card-body">
 
-                                            <div class="row g-3">
-                                                <div class="col-md">
-                                                    <div class="form-label">Wilayah</div>
-                                                    <select type="text" class="form-select" placeholder="Pilih Wilayah"
-                                                        name="kantor_kode" id="select-wilayah">
-                                                        <option value="">Pilih Wilayah</option>
-                                                        <option value="SBG">Subang</option>
-                                                        <option value="PGD">Pagaden</option>
-                                                        <option value="PMK">Pamanukan</option>
-                                                    </select>
-                                                </div>
-                                                <div class="col-md">
-                                                    <div class="form-label">Kasi Analis</div>
-                                                    <select type="text" class="form-select" placeholder="Pilih Kasi"
-                                                        name="kasi_kode" id="select-kasi">
-                                                        <option value="">Pilih Kasi</option>
-                                                        <option value="DDN">Dede Doni</option>
-                                                        <option value="RND">Rian Destila</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <p></p>
-                                            <div class="row g-3">
-                                                <div class="col-md">
-                                                    <div class="form-label">Survayor</div>
-                                                    <select type="text" class="form-select" placeholder="Pilih Survayor"
-                                                        name="survayor_kode" id="select-survayor">
-                                                        <option value="">Pilih Survayor</option>
-                                                        <option value="MHM">Muhidin</option>
-                                                        <option value="JAY">Jaelani</option>
-                                                    </select>
-                                                </div>
-                                                <div class="col-md">
-                                                    <div class="form-label">Nama CGC</div>
-                                                    <select type="text" class="form-select" placeholder="Pilih CGC"
-                                                        name="cgc_kode" id="select-cgc" value="">
-                                                        <option value="">Pilih CGC</option>
-                                                        <option value="0010101201">Yandi</option>
-                                                        <option value="0010101201">Zulfadli Rizal</option>
-                                                    </select>
-                                                </div>
-                                            </div>
+											<div class="row g-3">
+												<div class="col-md">
+													<div class="form-label">Wilayah</div>
+													<select type="text" class="form-select" placeholder="Pilih Wilayah"
+														name="kantor_kode" id="select-wilayah">
+														<option value="">Pilih Wilayah</option>
+														<option value="SBG">Subang</option>
+														<option value="PGD">Pagaden</option>
+														<option value="PMK">Pamanukan</option>
+													</select>
+												</div>
+												<div class="col-md">
+													<div class="form-label">Kasi Analis</div>
+													<select type="text" class="form-select" placeholder="Pilih Kasi"
+														name="kasi_kode" id="select-kasi">
+														<option value="">Pilih Kasi</option>
+														<option value="DDN">Dede Doni</option>
+														<option value="RND">Rian Destila</option>
+													</select>
+												</div>
+											</div>
+											<p></p>
+											<div class="row g-3">
+												<div class="col-md">
+													<div class="form-label">Survayor</div>
+													<select type="text" class="form-select" placeholder="Pilih Survayor"
+														name="survayor_kode" id="select-survayor">
+														<option value="">Pilih Survayor</option>
+														<option value="MHM">Muhidin</option>
+														<option value="JAY">Jaelani</option>
+													</select>
+												</div>
+												<div class="col-md">
+													<div class="form-label">Nama CGC</div>
+													<select type="text" class="form-select" placeholder="Pilih CGC"
+														name="tabungan_cgc" id="select-cgc" value="">
+														<option value="">Pilih CGC</option>
+														<option value="0010101201">Yandi</option>
+														<option value="0010101201">Zulfadli Rizal</option>
+													</select>
+												</div>
+											</div>
 
-                                        </div>
-                                        <div class="card-footer bg-transparent mt-auto">
-                                            <div class="btn-list justify-content-end">
-                                                <a href="#" class="btn btn-primary">
-                                                    Simpan
-                                                </a>
-                                            </div>
-                                        </div>
+										</div>
+										<div class="card-footer bg-transparent mt-auto">
+											<div class="btn-list justify-content-end">
+												<a href="#" class="btn btn-primary">
+													Simpan
+												</a>
+											</div>
+										</div>
 
-                                    </form>
+									</form>
 
-                                </div>
-                            </div>
-                        </div>
+								</div>
+							</div>
+						</div>
 
-                    </div>
+					</div>
 
-                </div>
-            </div>
+				</div>
+			</div>
 
-        </div>
-    </div>
+		</div>
+	</div>
 </div>
 @endsection
 
 @push('myscript')
 <script>
-    document.addEventListener("DOMContentLoaded", function () {
+	document.addEventListener("DOMContentLoaded", function () {
     	var el;
     	window.TomSelect && (new TomSelect(el = document.getElementById('select-status'), {
     		copyClassesToDropdown: false,
@@ -199,7 +199,7 @@
 
 @push('myscript')
 <script>
-    document.addEventListener("DOMContentLoaded", function () {
+	document.addEventListener("DOMContentLoaded", function () {
     	var el;
     	window.TomSelect && (new TomSelect(el = document.getElementById('select-kasi'), {
     		copyClassesToDropdown: false,
