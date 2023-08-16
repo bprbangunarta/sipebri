ab874c1451d3b78ef1d493ca4b06ed2c8ab24dbc
Zulfadli Rizal <zulfadlirizal@gmail.com>
2023-08-16 16:48:21 +0700

    fix layout

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -29,6 +29,7 @@
 <script src="{{ asset('tabler/dist/libs/jsvectormap/dist/maps/world.js?1674944402') }}" defer></script>
 <script src="{{ asset('tabler/dist/libs/jsvectormap/dist/maps/world-merc.js?1674944402') }}" defer></script>
 <script src="{{ asset('tabler/dist/libs/tom-select/dist/js/tom-select.base.min.js?1674944402') }}" defer></script>
+<script src="{{ asset('tabler/dist/libs/fslightbox/index.js?1674944402') }}" defer></script>
 
 <!-- Tabler Core -->
 <script src="{{ asset('tabler/dist/js/tabler.min.js?1674944402') }}" defer></script>
