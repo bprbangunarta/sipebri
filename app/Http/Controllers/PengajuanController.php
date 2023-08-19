7af52515a4076e4a1cebaf7cb6f84776ea43098d
Zulfadli Rizal <zulfadlirizal@gmail.com>
2023-08-19 20:53:21 +0700

    fix layout

--- app/Http/Controllers/PengajuanController.php
+++ app/Http/Controllers/PengajuanController.php
@@ -18,15 +18,16 @@ class PengajuanController extends Controller
 
     public function index(Request $request)
     {
-       $query = Pengajuan::join('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
-                ->select('data_pengajuan.kode_pengajuan as kode','data_pengajuan.nasabah_kode as kd_nasabah', 'data_pengajuan.plafon as plafon', 'data_pengajuan.jangka_waktu as jk', 'data_nasabah.nama_nasabah as nama', 'data_nasabah.alamat_ktp as alamat')->get();
+        $query = Pengajuan::join('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
+            ->select('data_pengajuan.kode_pengajuan as kode', 'data_pengajuan.nasabah_kode as kd_nasabah', 'data_pengajuan.plafon as plafon', 'data_pengajuan.jangka_waktu as jk', 'data_nasabah.nama_nasabah as nama', 'data_nasabah.alamat_ktp as alamat')->get();
 
-        return view('pengajuan.index',[
+        return view('pengajuan.index', [
             'data' => $query,
         ]);
     }
 
-    public function storepengajuan(Request $request){
+    public function storepengajuan(Request $request)
+    {
         // dd($request);
         $cek = $request->validate([
             'kode_pengajuan' => 'required',
@@ -41,7 +42,7 @@ class PengajuanController extends Controller
             'penggunaan' => 'required',
             'keterangan' => 'required',
         ]);
-        
+
         //Hapus format rupiah
         $remove = array("Rp", ".", " ");
         $cek['plafon'] = str_replace($remove, "", $cek['plafon']);
@@ -60,34 +61,34 @@ class PengajuanController extends Controller
         $nasabah = Nasabah::where('kode_nasabah', $req)->get();
 
         $pengajuan = Pengajuan::where('nasabah_kode', $nasabah[0]->kode_nasabah)->get();
-        
+
         //Data produk
         $produk = Data::produk($pengajuan[0]->produk_kode);
         $pengajuan[0]->produk_nama = $produk;
         $peng = $pengajuan[0];
-        
+
         // mencari nama
         $query = DB::connection('sqlsrv')
             ->table('resort')
             ->select('kode', 'ket')
             ->where('kode', $peng->resort_kode)
             ->first();
-            
+
 
         if (is_null($query)) {
-           $peng->nama_resort = null;
-        }else{
+            $peng->nama_resort = null;
+        } else {
             $peng->nama_resort = $query->ket;
         }
 
         //Data resort
         $resort = DB::connection('sqlsrv')
-                ->table('resort')
-                ->select('kode', 'ket')
-                ->get();
+            ->table('resort')
+            ->select('kode', 'ket')
+            ->get();
 
-     
-        return view('pengajuan.edit',[
+
+        return view('pengajuan.edit', [
             'data' => $nasabah,
             // 'resort' => $resort,
             'pengajuan' => $peng,
@@ -102,22 +103,22 @@ class PengajuanController extends Controller
 
         $pengajuan = Pengajuan::where('nasabah_kode', $cek->kode_nasabah)->get();
         $jaminan = DB::table('data_pengajuan')
-                    ->join('data_jaminan', 'data_pengajuan.kode_pengajuan', '=', 'data_jaminan.pengajuan_kode')
-                    ->join('data_jenis_agunan', 'data_jaminan.jenis_agunan_kode', '=', 'data_jenis_agunan.kode')
-                    ->join('data_jenis_dokumen', 'data_jaminan.jenis_dokumen_kode', '=', 'data_jenis_dokumen.kode')
-                    ->select('data_pengajuan.kode_pengajuan', 'data_jaminan.id', 'data_jaminan.no_dokumen', 'data_jaminan.atas_nama', 'data_jenis_agunan.jenis_agunan', 'data_jenis_dokumen.jenis_dokumen')
-                    ->where('data_pengajuan.kode_pengajuan', '=', $pengajuan[0]->kode_pengajuan)
-                    ->get();
+            ->join('data_jaminan', 'data_pengajuan.kode_pengajuan', '=', 'data_jaminan.pengajuan_kode')
+            ->join('data_jenis_agunan', 'data_jaminan.jenis_agunan_kode', '=', 'data_jenis_agunan.kode')
+            ->join('data_jenis_dokumen', 'data_jaminan.jenis_dokumen_kode', '=', 'data_jenis_dokumen.kode')
+            ->select('data_pengajuan.kode_pengajuan', 'data_jaminan.id', 'data_jaminan.no_dokumen', 'data_jaminan.atas_nama', 'data_jenis_agunan.jenis_agunan', 'data_jenis_dokumen.jenis_dokumen')
+            ->where('data_pengajuan.kode_pengajuan', '=', $pengajuan[0]->kode_pengajuan)
+            ->get();
 
         //Data agunan
         $agunan = Agunan::all();
-        
+
         //Data jenis dokumen
         $dok = DB::table('data_jenis_dokumen')->get();
 
         //Data dati
-        $kab = DB::select('select distinct kode_dati, nama_dati from v_dati'); 
-        
+        $kab = DB::select('select distinct kode_dati, nama_dati from v_dati');
+
         return view('pengajuan.agunan', [
             'agunan' => $agunan,
             'dok' => $dok,
@@ -128,8 +129,9 @@ class PengajuanController extends Controller
         ]);
     }
 
-    public function store(Request $request){
-        
+    public function store(Request $request)
+    {
+
         $cek = $request->validate([
             'pengajuan_kode' => 'required',
             'jenis_agunan_kode' => 'required',
@@ -145,7 +147,7 @@ class PengajuanController extends Controller
         // Merubah tanggal 
         $carbonDate = Carbon::createFromFormat('Y-m-d', $cek['masa_agunan']);
         $cek['masa_agunan'] = $carbonDate->format('Ymd');
-      
+
         try {
             DB::table('data_jaminan')->insert($cek);
             return redirect()->back()->with('success', 'Data berhasil ditambahkan');
@@ -154,52 +156,54 @@ class PengajuanController extends Controller
         }
     }
 
-    public function editagunan($id){
+    public function editagunan($id)
+    {
         $data = DB::table('data_jaminan')
-                ->where('id', $id)
-                ->get();
-        
+            ->where('id', $id)
+            ->get();
+
         //Ambil data agunan
         $agunan = Agunan::where('kode', $data[0]->jenis_agunan_kode)->get();
 
         //Ambil data dokumen
-        $dok= DB::table('data_jenis_dokumen')
-                ->select('jenis_dokumen')
-                ->where('kode', $data[0]->jenis_dokumen_kode)->get();
+        $dok = DB::table('data_jenis_dokumen')
+            ->select('jenis_dokumen')
+            ->where('kode', $data[0]->jenis_dokumen_kode)->get();
 
         //Data dati
         $dati = DB::table('v_dati')
-                ->select('nama_dati')
-                ->distinct()
-                ->where('kode_dati', $data[0]->kode_dati)->get();
+            ->select('nama_dati')
+            ->distinct()
+            ->where('kode_dati', $data[0]->kode_dati)->get();
 
         //Dati all data
         $kabupaten = DB::table('v_dati')
-                    ->select('kode_dati', 'nama_dati')
-                    ->distinct()->get(); 
+            ->select('kode_dati', 'nama_dati')
+            ->distinct()->get();
 
         //Agunan
         $agn = Agunan::select('kode', 'jenis_agunan')->get();
 
         //Dokumen
         $dokumen = DB::table('data_jenis_dokumen')
-                    ->select('kode', 'jenis_dokumen')->get();
-        
+            ->select('kode', 'jenis_dokumen')->get();
+
         //Format tanggal lahir
         $carbonDate = Carbon::createFromFormat('Ymd', $data[0]->masa_agunan);
-        $data[0]->masa_agunan = $carbonDate->format('m-d-Y');
+        $data[0]->masa_agunan = $carbonDate->format('Y-m-d');
 
         /* Menambahkan field baru ke variable data dari data agunan dan data dokumen */
         $data[0]->jenis_agunan = $agunan[0]->jenis_agunan;
         $data[0]->jenis_dokumen = $dok[0]->jenis_dokumen;
         $data[0]->nama_dati = $dati[0]->nama_dati;
 
-       
-        
+
+
         return response()->json([$data, $kabupaten, $agn, $dokumen]);
     }
 
-    public function updateagunan(Request $request){
+    public function updateagunan(Request $request)
+    {
         $cek = $request->validate([
             'jenis_agunan_kode' => 'required',
             'jenis_dokumen_kode' => 'required',
@@ -225,10 +229,11 @@ class PengajuanController extends Controller
         }
     }
 
-    public function destroy($pengajuan){
+    public function destroy($pengajuan)
+    {
         try {
-            DB::table('data_jaminan')->where('id', $pengajuan)->delete();   
-            return redirect()->back()->with('success', 'Data berhasil dihapus');           
+            DB::table('data_jaminan')->where('id', $pengajuan)->delete();
+            return redirect()->back()->with('success', 'Data berhasil dihapus');
         } catch (\Throwable $th) {
             return redirect()->back()->with('success', 'Data berhasil dihapus');
         }
