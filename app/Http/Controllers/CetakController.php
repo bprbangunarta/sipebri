74a70aaa30be661afd89fc0e9f44937818cbdd14
yandiyandhi <dev@bprbangunarta.co.id>
2023-08-28 14:13:52 +0700

    update

--- app/Http/Controllers/CetakController.php
+++ app/Http/Controllers/CetakController.php
@@ -2,136 +2,182 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Pekerjaan;
-use App\Models\Pendidikan;
-use App\Models\Pengajuan;
 use Carbon\Carbon;
+use App\Models\Pekerjaan;
+use App\Models\Pengajuan;
+use App\Models\Pendidikan;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Crypt;
-use Illuminate\Support\Facades\DB;
 use PhpParser\Node\Stmt\Return_;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Contracts\Encryption\DecryptException;
 
 class CetakController extends Controller
 {
     public function pengajuan(Request $request){
         $kode = $request->query('pengajuan');
-        $enc = Crypt::decrypt($kode);
-        $data = DB::table('data_pengajuan')
-                ->leftJoin('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
-                ->select('data_pengajuan.kode_pengajuan', 'data_pengajuan.plafon', 'data_nasabah.nama_nasabah', 'data_pengajuan.produk_kode', 'data_pengajuan.metode_rps', 'data_pengajuan.jangka_bunga', 'data_pengajuan.jangka_waktu')
-                ->where('data_pengajuan.kode_pengajuan', '=', $enc)->get();
         
-        $data[0]->kd_pengajuan = $kode;
-        
-        //Hari
-        $hari = Carbon::today();
-        $data[0]->hari = $hari->isoformat('D MMMM Y');
+        //====Try Enkripsi Request====//
+        try {
+            
+            $enc = Crypt::decrypt($kode);
+            $data = DB::table('data_pengajuan')
+                    ->leftJoin('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
+                    ->select('data_pengajuan.kode_pengajuan', 'data_pengajuan.plafon', 'data_nasabah.nama_nasabah', 'data_pengajuan.produk_kode', 'data_pengajuan.metode_rps', 'data_pengajuan.jangka_bunga', 'data_pengajuan.jangka_waktu')
+                    ->where('data_pengajuan.kode_pengajuan', '=', $enc)->get();
+            
+            $data[0]->kd_pengajuan = $kode;
+            
+            //Hari
+            $hari = Carbon::today();
+            $data[0]->hari = $hari->isoformat('D MMMM Y');
 
-        //Format Angka
-        $format_angka = "Rp. " . number_format($data[0]->plafon, 0, ',', '.');
-        $data[0]->rp_plafon = $format_angka;
+            //Format Angka
+            $format_angka = "Rp. " . number_format($data[0]->plafon, 0, ',', '.');
+            $data[0]->rp_plafon = $format_angka;
+            
+            return view('cetak.pengajuan',[
+                'data' => $data[0]
+            ]);
+
+        } catch (DecryptException $e) {
+            return abort(403, 'Anda gagal dalam berselancar');
+        }
         
-        return view('cetak.pengajuan',[
-            'data' => $data[0]
-        ]);
     }
 
     public function nik(Request $request){
         $kode = $request->query('cetak');
-        $enc = Crypt::decrypt($kode);
-        $data = DB::table('data_pengajuan')
-                ->leftJoin('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
-                ->select('data_nasabah.no_identitas', 'data_nasabah.nama_nasabah')
-                ->where('data_pengajuan.kode_pengajuan', '=', $enc)->get();
 
-        //Hari ini
-        $hari = Carbon::today();
-        $data[0]->hari = $hari->isoformat('D MMMM Y');
+        //====Try Enkripsi Request====//
+        try {
+            
+            $enc = Crypt::decrypt($kode);
+            $data = DB::table('data_pengajuan')
+                    ->leftJoin('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
+                    ->select('data_nasabah.no_identitas', 'data_nasabah.nama_nasabah')
+                    ->where('data_pengajuan.kode_pengajuan', '=', $enc)->get();
+
+            //Hari ini
+            $hari = Carbon::today();
+            $data[0]->hari = $hari->isoformat('D MMMM Y');
+            
+            return view('cetak.layouts.nik',[
+                'data' => $data[0]
+            ]);
+
+        } catch (DecryptException $e) {
+            return abort(403, 'Anda gagal dalam berselancar');
+        }
         
-        return view('cetak.layouts.nik',[
-            'data' => $data[0]
-        ]);
     }
 
     public function pendamping(Request $request){
         $kode = $request->query('cetak');
-        $enc = Crypt::decrypt($kode);
-        
-        $data = DB::table('data_pengajuan')
+
+        //====Try Enkripsi Request====//
+        try {
+
+            $enc = Crypt::decrypt($kode);
+            $data = DB::table('data_pengajuan')
                 ->leftJoin('data_pendamping', 'data_pengajuan.kode_pengajuan', '=', 'data_pendamping.pengajuan_kode')
                 ->leftJoin('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
                 ->where('data_pengajuan.kode_pengajuan', '=', $enc)
                 ->select('data_pendamping.no_identitas', 'data_pendamping.nama_pendamping', 'data_pendamping.tempat_lahir', 'data_pendamping.tanggal_lahir', 'data_nasabah.no_identitas as iden', 'data_nasabah.nama_nasabah', 'data_nasabah.tempat_lahir as tempat', 'data_nasabah.tanggal_lahir as ttl', 'data_nasabah.pendidikan_kode', 'data_nasabah.alamat_ktp')->get();
                 
 
-        //Rubah tanggal
-        $carbonDate = Carbon::createFromFormat('Ymd', $data[0]->tanggal_lahir);
-        $data[0]->tanggal_lahir = $carbonDate->isoformat('D MMMM Y');
-        $data[0]->tempat_lahir = ucfirst(strtolower($data[0]->tempat_lahir));
+            //Rubah tanggal
+            $carbonDate = Carbon::createFromFormat('Ymd', $data[0]->tanggal_lahir);
+            $data[0]->tanggal_lahir = $carbonDate->isoformat('D MMMM Y');
+            $data[0]->tempat_lahir = ucfirst(strtolower($data[0]->tempat_lahir));
 
-        //data pekerjaan
-        $job = Pendidikan::where('kode_pendidikan', $data[0]->pendidikan_kode)->get();
-        $data[0]->pendidikan_kode = $job[0]->nama_pendidikan;
+            //data pekerjaan
+            $job = Pendidikan::where('kode_pendidikan', $data[0]->pendidikan_kode)->get();
+            $data[0]->pendidikan_kode = $job[0]->nama_pendidikan;
+            
+            //Hari ini
+            $hari = Carbon::today();
+            $data[0]->hari = $hari->isoformat('dddd D MMMM YYYY');
+
+            return view('cetak.layouts.pendamping', [
+                'data' => $data[0]
+            ]);
+
+        } catch (DecryptException $e) {
+            return abort(403, 'Anda gagal dalam berselancar');
+        }
         
-        //Hari ini
-        $hari = Carbon::today();
-        $data[0]->hari = $hari->isoformat('dddd D MMMM YYYY');
-
-        return view('cetak.layouts.pendamping', [
-            'data' => $data[0]
-        ]);
     }
 
     public function motor(Request $request){
         $kode = $request->query('cetak');
-        $enc = Crypt::decrypt($kode);
-        $data = DB::table('data_pengajuan')
-                ->leftJoin('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
-                ->leftJoin('data_survei', 'data_pengajuan.kode_pengajuan', '=', 'data_survei.pengajuan_kode')
-                ->select('data_nasabah.no_identitas', 'data_nasabah.nama_nasabah', 'data_nasabah.tempat_lahir', 'data_nasabah.tanggal_lahir', 'data_nasabah.no_telp', 'data_nasabah.alamat_ktp', 'data_survei.surveyor_kode')
-                ->where('data_pengajuan.kode_pengajuan', '=', $enc)->get();
-        
-        //Surveyor
-        $surveyor = DB::table('v_users')    
-                    ->where('code_user', $data[0]->surveyor_kode)
-                    ->select('nama_user', 'role_name')->get();
 
-        //Tahun
-        $thn = Carbon::now()->year;
-        $data[0]->thn = $thn;
+        //====Try Enkripsi Request====//
+        try {
+            
+            $enc = Crypt::decrypt($kode);
+            $data = DB::table('data_pengajuan')
+                    ->leftJoin('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
+                    ->leftJoin('data_survei', 'data_pengajuan.kode_pengajuan', '=', 'data_survei.pengajuan_kode')
+                    ->select('data_nasabah.no_identitas', 'data_nasabah.nama_nasabah', 'data_nasabah.tempat_lahir', 'data_nasabah.tanggal_lahir', 'data_nasabah.no_telp', 'data_nasabah.alamat_ktp', 'data_survei.surveyor_kode')
+                    ->where('data_pengajuan.kode_pengajuan', '=', $enc)->get();
+            
+            //Surveyor
+            $surveyor = DB::table('v_users')    
+                        ->where('code_user', $data[0]->surveyor_kode)
+                        ->select('nama_user', 'role_name')->get();
+
+            //Tahun
+            $thn = Carbon::now()->year;
+            $data[0]->thn = $thn;
+            
+            $data[0]->nama_user = $surveyor[0]->nama_user;
+            $data[0]->role_name = $surveyor[0]->role_name;
+            
+            return view('cetak.layouts.motor',[
+                'data' =>$data[0]
+            ]);
+
+
+        } catch (DecryptException $e) {
+            return abort(403, 'Anda gagal dalam berselancar');
+        }
         
-        $data[0]->nama_user = $surveyor[0]->nama_user;
-        $data[0]->role_name = $surveyor[0]->role_name;
-        
-        return view('cetak.layouts.motor',[
-            'data' =>$data[0]
-        ]);
     }
 
     public function tanah(Request $request){
         $kode = $request->query('cetak');
-        $enc = Crypt::decrypt($kode);
 
-        $data = DB::table('data_pengajuan')
-                ->leftJoin('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
-                ->leftJoin('data_survei', 'data_pengajuan.kode_pengajuan', '=', 'data_survei.pengajuan_kode')
-                ->select('data_nasabah.no_identitas', 'data_nasabah.nama_nasabah', 'data_nasabah.tempat_lahir', 'data_nasabah.tanggal_lahir', 'data_nasabah.no_telp', 'data_nasabah.alamat_ktp', 'data_survei.surveyor_kode')
-                ->where('data_pengajuan.kode_pengajuan', '=', $enc)->get();
+        //====Try Enkripsi Request====//
+        try {
+            
+            $enc = Crypt::decrypt($kode);
 
-        //Surveyor
-        $surveyor = DB::table('v_users')    
-                    ->where('code_user', $data[0]->surveyor_kode)
-                    ->select('nama_user', 'role_name')->get();
+            $data = DB::table('data_pengajuan')
+                    ->leftJoin('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
+                    ->leftJoin('data_survei', 'data_pengajuan.kode_pengajuan', '=', 'data_survei.pengajuan_kode')
+                    ->select('data_nasabah.no_identitas', 'data_nasabah.nama_nasabah', 'data_nasabah.tempat_lahir', 'data_nasabah.tanggal_lahir', 'data_nasabah.no_telp', 'data_nasabah.alamat_ktp', 'data_survei.surveyor_kode')
+                    ->where('data_pengajuan.kode_pengajuan', '=', $enc)->get();
 
-        $data[0]->nama_user = $surveyor[0]->nama_user;
-        $data[0]->role_name = $surveyor[0]->role_name;
+            //Surveyor
+            $surveyor = DB::table('v_users')    
+                        ->where('code_user', $data[0]->surveyor_kode)
+                        ->select('nama_user', 'role_name')->get();
 
-        //Tahun
-        $thn = Carbon::now()->year;
-        $data[0]->thn = $thn;
+            $data[0]->nama_user = $surveyor[0]->nama_user;
+            $data[0]->role_name = $surveyor[0]->role_name;
+
+            //Tahun
+            $thn = Carbon::now()->year;
+            $data[0]->thn = $thn;
+            
+            return view('cetak.layouts.tanah', [
+                'data' => $data[0]
+            ]);
+
+        } catch (DecryptException $e) {
+            return abort(403, 'Anda gagal dalam berselancar');
+        }
         
-        return view('cetak.layouts.tanah', [
-            'data' => $data[0]
-        ]);
     }
 }
