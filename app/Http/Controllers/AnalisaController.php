490848fd4daeb0054ec8d79146a521ca77bcdddb
yandiyandhi <dev@bprbangunarta.co.id>
2023-08-31 10:46:56 +0700

    update

--- app/Http/Controllers/AnalisaController.php
+++ app/Http/Controllers/AnalisaController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Midle;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
 
 class AnalisaController extends Controller
@@ -15,11 +17,61 @@ class AnalisaController extends Controller
                 ->leftJoin('data_survei', 'data_pengajuan.kode_pengajuan', '=', 'data_survei.pengajuan_kode')
                 ->leftJoin('data_kantor', 'data_survei.kantor_kode', '=', 'data_kantor.kode_kantor')
                 ->leftJoin('users', 'data_survei.surveyor_kode', '=', 'users.code_user')
-                // ->where('data_survei.surveyor_kode', '=', $usr)
+                ->where('data_survei.surveyor_kode', '=', 'MSP')
                 ->select('data_pengajuan.kode_pengajuan', 'data_nasabah.kode_nasabah', 'data_nasabah.nama_nasabah', 'data_nasabah.alamat_ktp', 'data_pengajuan.plafon', 'data_kantor.nama_kantor', 'data_survei.surveyor_kode', 'data_survei.tgl_survei', 'data_survei.tgl_jadul_1', 'data_survei.tgl_jadul_2','users.name')->get();
+        
+        //Enkripsi kode pengajuan
+        $cek[0]->kd_pengajuan = Crypt::encrypt($cek[0]->kode_pengajuan);
         
         return view('analisa.proses', [
             'data' => $cek
         ]);
     }
+
+    public function analisa_usaha_perdagangan(Request $request){
+        $enc = Crypt::decrypt($request->query('pengajuan'));
+        $cek = Midle::analisa_usaha($enc);
+        
+        
+        return view('analisa.usaha.perdagangan', [
+            'data' => $cek[0]
+        ]);
+    }
+
+     public function analisa_usaha_pertanian(Request $request){
+        $enc = Crypt::decrypt($request->query('pengajuan'));
+        $cek = Midle::analisa_usaha($enc);
+       
+        return view('analisa.usaha.pertanian', [
+            'data' => $cek[0]
+        ]);
+     }
+     
+     public function analisa_usaha_jasa(Request $request){
+        $enc = Crypt::decrypt($request->query('pengajuan'));
+        $cek = Midle::analisa_usaha($enc);
+        
+        return view('analisa.usaha.jasa', [
+            'data' => $cek[0]
+        ]);
+     }
+
+     
+     public function analisa_usaha_lainnya(Request $request){
+        $enc = Crypt::decrypt($request->query('pengajuan'));
+        $cek = Midle::analisa_usaha($enc);
+        
+        return view('analisa.usaha.lainnya', [
+            'data' => $cek[0]
+        ]);
+     }
+     
+      public function analisa_keuangan(Request $request){
+        $enc = Crypt::decrypt($request->query('pengajuan'));
+        $cek = Midle::analisa_usaha($enc);
+        
+        return view('analisa.keuangan', [
+            'data' => $cek[0]
+        ]);
+      }
 }
