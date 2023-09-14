7d4e92e1370d9afac20c683fe4dc798ad4959004
yandiyandhi <dev@bprbangunarta.co.id>
2023-09-14 15:50:36 +0700

    update

--- resources/views/analisa/penjadwalan.blade.php
+++ resources/views/analisa/penjadwalan.blade.php
@@ -143,55 +143,6 @@
                                 <p></p>
 
                                 {{ $data->links('vendor.pagination.bootstrap-5') }}
-                                {{-- <nav class="d-flex justify-items-center justify-content-between">
-                                    <div class="d-flex justify-content-between flex-fill d-sm-none">
-                                        <ul class="pagination">
-                                            <li class="page-item disabled" aria-disabled="true">
-                                                <span class="page-link">« Previous</span>
-                                            </li>
-                                            <li class="page-item">
-                                                <a class="page-link"
-                                                    href="https://presensi.bprbangunarta.co.id/karyawan?page=2"
-                                                    rel="next">Next
-                                                    »</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-
-                                    <div
-                                        class="d-none flex-sm-fill d-sm-flex align-items-sm-center justify-content-sm-between">
-                                        <div>
-                                            <p class="small text-muted">
-                                                Showing
-                                                <span class="fw-semibold">1</span>
-                                                to
-                                                <span class="fw-semibold">10</span>
-                                                of
-                                                <span class="fw-semibold">54</span>
-                                                results
-                                            </p>
-                                        </div>
-
-                                        <div>
-                                            <ul class="pagination">
-                                                <li class="page-item disabled" aria-disabled="true"
-                                                    aria-label="« Previous">
-                                                    <span class="page-link" aria-hidden="true">‹</span>
-                                                </li>
-                                                <li class="page-item active" aria-current="page">
-                                                    <span class="page-link">1</span>
-                                                </li>
-                                                <li class="page-item">
-                                                    <a class="page-link" href="#">2</a>
-                                                </li>
-                                                <li class="page-item">
-                                                    <a class="page-link" href="#" rel="next"
-                                                        aria-label="Next »">›</a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </nav> --}}
 
                             </div>
                         </div>
