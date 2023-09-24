bc5207581f94962ece9faf029d852f74d475c9d7
Zulfadli Rizal <dev@bprbangunarta.co.id>
2023-09-24 21:48:39 +0700

    Fix Tabungan

--- app/Http/Controllers/TabunganController.php
+++ app/Http/Controllers/TabunganController.php
@@ -12,10 +12,7 @@ class TabunganController extends Controller
 {
     public function index()
     {
-        $query = Tabungan::query();
-        $query->select('*');
-
-        $tabungan = $query->paginate(10);
+        $tabungan = Tabungan::latest()->paginate(10);
         return view('tabungan', compact('tabungan'));
     }
 }
