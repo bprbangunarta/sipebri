5b6b9324f5a49daba79cbfa3cfdd73e52d4de957
yandiyandhi <dev@bprbangunarta.co.id>
2023-10-11 17:04:05 +0700

    update

--- app/Http/Controllers/CetakAnalisaController.php
+++ app/Http/Controllers/CetakAnalisaController.php
@@ -243,6 +243,7 @@ class CetakAnalisaController extends Controller
         try {
             $enc = Crypt::decrypt($kode);
             $data = Midle::memorandum($enc);
+
             return view('cetak.analisa.memorandum_usulan');
         } catch (DecryptException $e) {
             return abort(403, 'Permintaan anda di Tolak.');
