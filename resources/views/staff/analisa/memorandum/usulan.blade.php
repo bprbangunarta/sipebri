3022ddba4e19069d97b3736b1b67a0aba08df9cc
Zulfadli Rizal <dev@bprbangunarta.co.id>
2023-10-17 21:36:01 +0700

    Memorandum

--- resources/views/staff/analisa/memorandum/usulan.blade.php
+++ resources/views/staff/analisa/memorandum/usulan.blade.php
@@ -13,37 +13,27 @@
                     <div class="div-left">
                         <div style="width: 100%;float:left;">
                             <span class="fw-bold">MAX PLAFOND</span>
-                            <input type="text" class="form-control text-uppercase" name="ket_kewajiban1" id="" value="RP." readonly>
+                            <input type="text" class="form-control text-uppercase" name="max_plafond" id="" value="RP." readonly>
                         </div>
 
                         <div style="margin-top:5px;width: 49.5%;float:left;">
                             <span class="fw-bold">USULAN PLAFOND</span>
-                            <select class="form-control input-sm form-border text-uppercase bi_penggunaan_kode" name="bi_penggunaan_kode" id="">
-                                <option value="">--Pilih--</option>
-                                {{-- Ambil data pada tabel bi_penggunaan_debitur --}}
-                                <option value="10">10 - Modal Kerja</option>
-                                <option value="20">20 - Investasi</option>
-                            </select>
+                            <input type="text" class="form-control text-uppercase" name="usulan_plafond" id="" placeholder="RP.">
                         </div>
 
                         <div style="margin-top:5px;width: 49.5%;float:right;">
                             <span class="fw-bold">JANGKA WAKTU</span>
-                            <select class="form-control input-sm text-uppercase bi_gol_penjamin_kode" name="bi_gol_penjamin_kode" id="">
-                                <option value="">--Pilih--</option>
-                                {{-- Ambil data pada tabel bi_golongan_penjamin --}}
-                                <option value="000">000 - Tanpa Penjamin</option>
-                                <option value="880">880 - Asuransi Jiwa</option>
-                            </select>
+                            <input type="number" class="form-control text-uppercase" name="jangka_waktu" id="" placeholder="0">
                         </div>
 
                         <div style="margin-top:5px;width: 49.5%;float:left;">
                             <span class="fw-bold">REPAYMENT CAPACITY</span>
-                            <input type="text" class="form-control text-uppercase" name="ket_kewajiban1" id="" value="00.00%" readonly>
+                            <input type="text" class="form-control text-uppercase" value="00.00%" readonly>
                         </div>
 
                         <div style="margin-top:5px;width: 49.5%;float:right;">
                             <span class="fw-bold">TAKSASI AGUNAN</span>
-                            <input type="text" class="form-control text-uppercase" name="ket_kewajiban1" id="" value="00.00%" readonly>
+                            <input type="text" class="form-control text-uppercase" value="00.00%" readonly>
                         </div>
                     </div>
 
@@ -51,17 +41,17 @@
                     <div class="div-right">
                         <div style="width: 100%;float:left;">
                             <span class="fw-bold">SEBELUM REALISASI</span>
-                            <input type="text" class="form-control text-uppercase" name="ket_kewajiban1" id="" placeholder="ENTRI">
+                            <input type="text" class="form-control text-uppercase" name="sebelum_realisasi" id="" placeholder="ENTRI">
                         </div>
 
                         <div style="margin-top:5px;width: 100%;float:right;">
                             <span class="fw-bold">SYARAT TAMBAHAN</span>
-                            <input type="text" class="form-control text-uppercase" name="ket_kewajiban1" id="" placeholder="ENTRI">
+                            <input type="text" class="form-control text-uppercase" name="syarat_tambahan" id="" placeholder="ENTRI">
                         </div>
 
                         <div style="margin-top:5px;width: 100%;float:left;">
                             <span class="fw-bold">SYARAT LAINNYA</span>
-                            <input type="text" class="form-control text-uppercase" name="ket_kewajiban1" id="" placeholder="ENTRI">
+                            <input type="text" class="form-control text-uppercase" name="syarat_lainnya" id="" placeholder="ENTRI">
                         </div>
                     </div>
 
