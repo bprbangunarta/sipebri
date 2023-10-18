3d4e923ec5b4c5d97c59aecd6ce4b1ab57c1d3cd
yandiyandhi <dev@bprbangunarta.co.id>
2023-10-18 09:32:19 +0700

    update

--- public/assets/js/myscript/persetujuan_komite.js
+++ public/assets/js/myscript/persetujuan_komite.js
@@ -3,11 +3,33 @@ $(document).ready(function () {
         var button = $(event.relatedTarget);
         var pengajuan = button.data("pengajuan");
         var token = $('meta[name="csrf-token"]').attr("content");
-        console.log(button);
+
         var token = $('meta[name="csrf-token"]').attr("content");
         var data = {
             field: pengajuan,
             _token: token,
         };
+        $.ajax({
+            url: "/themes/komite/kredit/data",
+            type: "post",
+            dataType: "json",
+            cache: false,
+            data: data,
+            success: function (response) {
+                var da = JSON.stringify(response);
+                var data = JSON.parse(da);
+                var hasil = data[0];
+                $("#kode").val(hasil.kode_pengajuan);
+                $("#nama").val(hasil.nama_nasabah);
+
+                var pal = hasil.plafon;
+                var pl = "Rp. " + pal.toLocaleString("id-ID");
+                $("#plafon").val(pl);
+            },
+            error: function (xhr, status, error) {
+                // Tindakan jika terjadi kesalahan dalam permintaan AJAX
+                console.error("Error:", xhr.responseText);
+            },
+        });
     });
 });
