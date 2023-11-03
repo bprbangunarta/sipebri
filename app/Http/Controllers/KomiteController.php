8809441685d77c8c92c91282445beea0d532e583
Zulfadli Rizal <dev@bprbangunarta.co.id>
2023-11-03 15:07:17 +0700

    Fix

--- app/Http/Controllers/KomiteController.php
+++ app/Http/Controllers/KomiteController.php
@@ -223,7 +223,28 @@ class KomiteController extends Controller
             ->orWhere('data_pengajuan.tracking', '=', "Naik Kasi")
             ->orWhere('data_pengajuan.tracking', '=', "Naik Komite I")
             ->orWhere('data_pengajuan.tracking', '=', "Naik Komite II")
-            ->select('data_pengajuan.kode_pengajuan', 'data_pengajuan.tracking', 'data_pengajuan.status', 'data_pengajuan.plafon', 'data_pengajuan.created_at', 'data_pengajuan.kategori', 'data_nasabah.kode_nasabah', 'data_nasabah.nama_nasabah', 'data_nasabah.alamat_ktp', 'data_nasabah.kelurahan', 'data_nasabah.kecamatan', 'data_pengajuan.plafon', 'data_kantor.nama_kantor', 'data_survei.surveyor_kode', 'data_survei.tgl_survei', 'data_survei.tgl_jadul_1', 'data_survei.tgl_jadul_2', 'users.name');
+            ->select(
+                'data_pengajuan.kode_pengajuan',
+                'data_pengajuan.tracking',
+                'data_pengajuan.status',
+                'data_pengajuan.plafon',
+                'data_pengajuan.produk_kode',
+                'data_pengajuan.jangka_waktu as jk',
+                'data_pengajuan.created_at',
+                'data_pengajuan.kategori',
+                'data_nasabah.kode_nasabah',
+                'data_nasabah.nama_nasabah',
+                'data_nasabah.alamat_ktp',
+                'data_nasabah.kelurahan',
+                'data_nasabah.kecamatan',
+                'data_pengajuan.plafon',
+                'data_kantor.kode_kantor',
+                'data_survei.surveyor_kode',
+                'data_survei.tgl_survei',
+                'data_survei.tgl_jadul_1',
+                'data_survei.tgl_jadul_2',
+                'users.name'
+            );
         //
         $c = $cek->get();
         $count = count($c);
--- resources/views/pengajuan/all.blade.php
+++ resources/views/pengajuan/all.blade.php
@@ -2,113 +2,109 @@
 @section('title', 'Data Pengajuan')
 @yield('jquery')
 @section('content')
-    <div class="content-wrapper">
-        <section class="content">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="box box-primary">
-                        <div class="box-header with-border">
-                            <i class="fa fa-user"></i>
-                            <h3 class="box-title">DATA PENGAJUAN KREDIT</h3>
+<div class="content-wrapper">
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <i class="fa fa-user"></i>
+                        <h3 class="box-title">DATA PENGAJUAN KREDIT</h3>
 
-                            <div class="box-tools">
-                                <form {{ route('user.index') }}" method="GET">
-                                    <div class="input-group input-group-sm hidden-xs" style="width: 170px;">
-                                        <input type="text" class="form-control pull-right" name="name" id="name"
-                                            value="{{ request('name') }}" placeholder="Search">
+                        <div class="box-tools">
+                            <form {{ route('user.index') }}" method="GET">
+                                <div class="input-group input-group-sm hidden-xs" style="width: 170px;">
+                                    <input type="text" class="form-control pull-right" name="name" id="name" value="{{ request('name') }}" placeholder="Search">
 
-                                        <div class="input-group-btn">
-                                            <button type="submit" class="btn btn-default"><i
-                                                    class="fa fa-search"></i></button>
-                                        </div>
+                                    <div class="input-group-btn">
+                                        <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
                                     </div>
-                                </form>
-                            </div>
+                                </div>
+                            </form>
                         </div>
-
-                        <div class="box-body">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr class="bg-blue">
-                                        <th class="text-center" width="3%">NO</th>
-                                        <th class="text-center">INFORMASI NASABAH</th>
-                                        <th class="text-center" width="40%">ALAMAT</th>
-                                        <th class="text-center" width="17%">PENGAJUAN</th>
-                                        <th class="text-center" width="10%">STATUS</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @php
-                                        $no = 1;
-                                    @endphp
-                                    @forelse ($data as $item)
-                                        <tr>
-                                            <td class="text-center" style="vertical-align: middle;">
-                                                {{ $loop->iteration + $data->firstItem() - 1 }}
-                                            </td>
-                                            
-                                            <td style="vertical-align: middle;">
-                                                <b>KODE :</b> {{ $item->kode }} [ {{ $item->kategori }} ] <br>
-                                                <b>NAMA :</b> {{ strtoupper($item->nama) }} <br>
-                                                <b>TANGGAL :</b> {{ \Carbon\Carbon::parse($item->tanggal)->format('Y-m-d') }}
-                                            </td>
-
-                                            @if (is_null($item->alamat))
-                                                <td class="text-center" style="vertical-align: middle;">-</td>
-                                            @else
-                                                <td class="text-uppercase" style="vertical-align: middle;">{{ $item->alamat }} <br>
-                                                    <b>Desa: </b>{{ $item->kelurahan }} | <b>Kecamatan:
-                                                    </b>{{ $item->kecamatan }}
-                                                </td>
-                                            @endif
-
-                                            @php
-                                                $item->plafon = number_format($item->plafon, 0, ',', '.');
-                                            @endphp
-                                            <td style="vertical-align: middle;">
-                                                <b>KANTOR :</b> {{ $item->kantor }} <br>
-                                                <b>{{ $item->produk_kode }} - JK :</b> {{ $item->jk }} BULAN <br>
-                                                <b>PLAFON :</b> {{ $item->plafon }}
-                                            </td>
-
-                                            <td class="text-center" style="vertical-align: middle;">
-                                                @if ($item->status == 'Lengkapi Data' || $item->status == 'Minta Otorisasi')
-                                                    <span class="btn bg-blue" style="width: 120px;hight:100%;">Verifikasi
-                                                        Data</span>
-                                                @elseif ($item->status == 'Batal' || $item->status == 'Dibatalkan' || $item->status == 'Ditolak')
-                                                    <span class="btn bg-red"
-                                                        style="width: 120px;hight:100%;">{{ $item->status }}</span>
-                                                @elseif ($item->status == 'Sudah Otorisasi')
-                                                    <span class="btn bg-yellow" style="width: 120px;hight:100%;">Survey &
-                                                        Analisa</span>
-                                                @else
-                                                    <span class="btn bg-green"
-                                                        style="width: 120px;hight:100%;">{{ $item->status }}</span>
-                                                @endif
-
-                                                <p style="margin-top:-5px;"></p>
-                                                <span class="btn bg-purple" style="width: 120px;hight:100%;">Lihat Tracking</span>
-                                            </td>
-                                        </tr>
-                                        @php
-                                            $no++;
-                                        @endphp
-                                    @empty
-                                        <tr>
-                                            <td class="text-center" colspan="7">TIDAK ADA DATA</td>
-                                        </tr>
-                                    @endforelse
-                                </tbody>
-                            </table>
-                        </div>
-
-                        <div class="box-footer clearfix">
-                            {{ $data->links('vendor.pagination.adminlte') }}
-                        </div>
-
                     </div>
+
+                    <div class="box-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr class="bg-blue">
+                                    <th class="text-center" width="3%">NO</th>
+                                    <th class="text-center">INFORMASI NASABAH</th>
+                                    <th class="text-center" width="40%">ALAMAT</th>
+                                    <th class="text-center" width="17%">PENGAJUAN</th>
+                                    <th class="text-center" width="10%">STATUS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php
+                                $no = 1;
+                                @endphp
+                                @forelse ($data as $item)
+                                <tr>
+                                    <td class="text-center" style="vertical-align: middle;">
+                                        {{ $loop->iteration + $data->firstItem() - 1 }}
+                                    </td>
+
+                                    <td style="vertical-align: middle;">
+                                        <b>KODE :</b> {{ $item->kode }} [ {{ $item->kategori }} ] <br>
+                                        <b>NAMA :</b> {{ strtoupper($item->nama) }} <br>
+                                        <b>TANGGAL :</b> {{ \Carbon\Carbon::parse($item->tanggal)->format('Y-m-d') }}
+                                    </td>
+
+                                    @if (is_null($item->alamat))
+                                    <td class="text-center" style="vertical-align: middle;">-</td>
+                                    @else
+                                    <td class="text-uppercase" style="vertical-align: middle;">{{ $item->alamat }} <br>
+                                        <b>Desa: </b>{{ $item->kelurahan }} | <b>Kecamatan:
+                                        </b>{{ $item->kecamatan }}
+                                    </td>
+                                    @endif
+
+                                    @php
+                                    $item->plafon = number_format($item->plafon, 0, ',', '.');
+                                    @endphp
+                                    <td style="vertical-align: middle;">
+                                        <b>KANTOR :</b> {{ $item->kantor }} <br>
+                                        <b>{{ $item->produk_kode }} - JK :</b> {{ $item->jk }} BULAN <br>
+                                        <b>PLAFON :</b> {{ $item->plafon }}
+                                    </td>
+
+                                    <td class="text-center" style="vertical-align: middle;">
+                                        @if ($item->status == 'Lengkapi Data' || $item->status == 'Minta Otorisasi')
+                                        <span class="btn bg-blue" style="width: 120px;hight:100%;">Verifikasi
+                                            Data</span>
+                                        @elseif ($item->status == 'Batal' || $item->status == 'Dibatalkan' || $item->status == 'Ditolak')
+                                        <span class="btn bg-red" style="width: 120px;hight:100%;">{{ $item->status }}</span>
+                                        @elseif ($item->status == 'Sudah Otorisasi')
+                                        <span class="btn bg-yellow" style="width: 120px;hight:100%;">Survey &
+                                            Analisa</span>
+                                        @else
+                                        <span class="btn bg-green" style="width: 120px;hight:100%;">{{ $item->status }}</span>
+                                        @endif
+
+                                        <p style="margin-top:-5px;"></p>
+                                        <span class="btn bg-purple" style="width: 120px;hight:100%;">Lihat Tracking</span>
+                                    </td>
+                                </tr>
+                                @php
+                                $no++;
+                                @endphp
+                                @empty
+                                <tr>
+                                    <td class="text-center" colspan="7">TIDAK ADA DATA</td>
+                                </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="box-footer clearfix">
+                        {{ $data->links('vendor.pagination.adminlte') }}
+                    </div>
+
                 </div>
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
+</div>
 @endsection
