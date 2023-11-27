2cafcb810cb559122925d6215965acdbb4ae89cf
MARKOM-PC <dev@bprbangunarta.co.id>
2023-11-27 11:59:39 +0700

    Fix

--- resources/views/cetak/layouts/nik.blade.php
+++ resources/views/cetak/layouts/nik.blade.php
@@ -8,7 +8,10 @@
     <style>
         @page {
             size: A4;
-            margin: 0;
+            margin-top: 1.0cm;
+            margin-bottom: 0.5cm;
+            margin-left: 0cm;
+            margin-right: 0cm;
         }
 
         body {
@@ -20,7 +23,7 @@
         table {
             width: 100%;
             border-collapse: collapse;
-            /* border: 2px solid #000000; */
+            /* border: 1px solid #000000; */
             /* Menambahkan border ke seluruh tabel */
         }
 
@@ -28,9 +31,12 @@
         td {
             padding: 1px;
             text-align: left;
-            /* border-bottom: 1px solid #000000;
-            border-right: 1px solid #000000; */
             /* Menambahkan border pada sisi kanan sel */
+        }
+
+        .br-1 {
+            border-bottom: 1px solid #000000;
+            border-right: 1px solid #000000;
         }
 
         th:last-child,
@@ -56,7 +62,6 @@
             max-width: 100%;
             padding: 20px;
             box-sizing: border-box;
-            margin-top: -10px;
             text-align: justify;
         }
 
@@ -66,21 +71,77 @@
             }
 
             .content {
-                padding: 1.5cm;
+                padding-top: 1.5cm;
+                padding-bottom: 1.5cm;
+                padding-left: 2cm;
+                padding-right: 2cm;
             }
         }
     </style>
 </head>
 
 <body>
-    <div class="content">
+    <!-- Halaman 1 -->
+    <div class="content" style="margin-top: -57px;">
 
         <img src="{{ asset('assets/img/pba.png') }}" style="width:200px;">
         <hr style="border: 1px solid 034871;">
 
-        <h4 style="text-align: center;">SURAT PERNYATAAN</h4>
+        <h4 style="text-align: center;">SURAT PERNYATAAN PEMOHON</h4>
 
-        <P>Yang bertanda tangan dibawah ini :</P>
+        <p>Yang bertanda tangan dibawah ini :</p>
+
+        <table>
+            <tr>
+                <td style="width: 5%;"></td>
+                <td>NIK</td>
+                <td class="text-center"> : </td>
+                <td>{{ $data->no_identitas }}</td>
+
+            </tr>
+            <tr>
+                <td style="width: 5%;"></td>
+                <td style="width:5%;">Nama</td>
+                <td class="text-center" style="width: 3%;"> : </td>
+                <td>{{ $data->nama_nasabah }}</td>
+            </tr>
+        </table>
+
+        <p>
+            Dengan ini saya menyatakan dan menyetujui untuk dilakukan pengecekan NIK, secara online yang terhubung
+            dengan sistem Ditjen Dukcapil, maupun secara offline (alat baca card reader), dan hasil pengecekan akan
+            dikelola sebagaimana mestinya untuk keperluan BPR/BPRS PT BPR BANGUNARTA.
+        </p>
+
+        <p>
+            Demikian Surat Pernyataan ini dibuat untuk dapat dipergunakan sebagaimana mestinya.
+        </p>
+
+        <p style="float: right;">Pamanukan, {{ $data->hari }}</p>
+        <br>
+
+        <table>
+            <tr>
+                <td class="text-center">
+                    Yang Menyatakan,<br><br><br><br><br>
+
+                    <font style="font-weight: bold;text-decoration: underline;">{{ $data->nama_nasabah }}</font>
+                </td>
+                <td class="text-center">
+                    Petugas Pengecek NIK,<br><br><br><br><br>
+
+                    <font style="font-weight: bold;text-decoration: underline;text-transform:uppercase;">Unun Nurainun
+                    </font>
+                </td>
+            </tr>
+        </table>
+
+        <br>
+        <hr>
+
+        <h4 style="text-align: center;">SURAT PERNYATAAN PENDAMPING</h4>
+
+        <p>Yang bertanda tangan dibawah ini :</p>
 
         <table>
             <tr>
--- resources/views/cetak/layouts/slik.blade.php
+++ resources/views/cetak/layouts/slik.blade.php
@@ -8,7 +8,10 @@
     <style>
         @page {
             size: A4;
-            margin: 0;
+            margin-top: 1.0cm;
+            margin-bottom: 0.5cm;
+            margin-left: 0cm;
+            margin-right: 0cm;
         }
 
         body {
@@ -20,7 +23,7 @@
         table {
             width: 100%;
             border-collapse: collapse;
-            /* border: 2px solid #000000; */
+            /* border: 1px solid #000000; */
             /* Menambahkan border ke seluruh tabel */
         }
 
@@ -28,9 +31,12 @@
         td {
             padding: 1px;
             text-align: left;
-            /* border-bottom: 1px solid #000000;
-            border-right: 1px solid #000000; */
             /* Menambahkan border pada sisi kanan sel */
+        }
+
+        .br-1 {
+            border-bottom: 1px solid #000000;
+            border-right: 1px solid #000000;
         }
 
         th:last-child,
@@ -43,9 +49,8 @@
             background-color: #f2f2f2;
         }
 
-        input,
-        textarea {
-            border: 1px solid #000000;
+        input {
+            border: none;
         }
 
         .text-center {
@@ -57,8 +62,7 @@
             max-width: 100%;
             padding: 20px;
             box-sizing: border-box;
-            margin-top: -10px;
-            /* text-align: justify; */
+            text-align: justify;
         }
 
         @media print {
@@ -67,19 +71,102 @@
             }
 
             .content {
-                padding: 1.5cm;
+                padding-top: 1.5cm;
+                padding-bottom: 1.5cm;
+                padding-left: 2cm;
+                padding-right: 2cm;
             }
         }
     </style>
 </head>
 
 <body>
-    <div class="content">
+    <!-- Halaman 1 -->
+    <div class="content" style="margin-top: -57px;">
 
         <img src="{{ asset('assets/img/pba.png') }}" style="width:200px;">
         <hr style="border: 1px solid 034871;">
 
         <h4 style="text-align: center;">FORMULIR PERMOHONAN INFORMASI DEBITUR (IDEB) PERSEORANGAN</h4>
+
+        <p>Yang bertanda tangan dibawah ini :</p>
+
+        <table>
+            <tr>
+                <td>Nomot KTP</td>
+                <td class="text-center"> : </td>
+                <td>{{ $data->no_identitas }}</td>
+            </tr>
+            <tr>
+                <td style="width: 23%;">Nama Sesuai Identitas</td>
+                <td class="text-center" style="width: 3%;"> : </td>
+                <td>{{ $data->nama_nasabah }}</td>
+            </tr>
+            <tr>
+                <td>Tempat. Tanggal Lahir</td>
+                <td class="text-center"> : </td>
+                <td>{{ $data->tempat_lahir . ',' . ' ' . $data->tanggal_lahir }}</td>
+            </tr>
+            <tr>
+                <td>Nomor Telepon</td>
+                <td class="text-center"> : </td>
+                <td>{{ $data->no_telp }}</td>
+            </tr>
+            <tr>
+                <td style="position: absolute;vertical-align: text-top;">Alamat Sesuai Identitas</td>
+                <td class="text-center" style="vertical-align: text-top;"> : </td>
+                <td>
+                    {{ $data->alamat_ktp }}
+                </td>
+            </tr>
+            <tr>
+                <td style="position: absolute;">Tujuan Penggunaan</td>
+                <td class="text-center"> : </td>
+                <td>
+                    <input type="checkbox"> Pemeriksaan Calon Debitur&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="checkbox"> Dalam Rangka Audit<br>
+
+                    <input type="checkbox"> Monitoring Debitr Existing&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="checkbox"> Penanganan Pengaduan Debitur<br>
+
+                    <input type="checkbox"> Melayani Permintaan Debitur
+                    <input type="checkbox"> Penilaian Karyawan/Calon Kayyawan
+                </td>
+            </tr>
+        </table>
+
+        <p style="float: right;">Pamanukan, {{ $data->hari }}</p>
+        <br>
+
+        <table>
+            <tr>
+                <td class="text-center">
+                    Pelaksana IDEB,<br><br><br><br><br>
+
+                    <font style="font-weight: bold;text-decoration: underline;text-transform:uppercase;">Unun Nurainun
+                    </font>
+                </td>
+                <td class="text-center">
+                    Menyetujui,<br><br><br><br><br>
+
+                    <font style="font-weight: bold;text-decoration: underline;text-transform:uppercase;">{{
+                        $data->kasi_kode }}</font>
+                </td>
+                <td class="text-center">
+                    Pemohon,<br><br><br><br><br>
+
+                    <font style="font-weight: bold;text-decoration: underline;text-transform:uppercase;">{{
+                        $data->surveyor_kode }}</font>
+                </td>
+            </tr>
+        </table>
+
+        <br>
+        <hr>
+
+        <h4 style="text-align: center;">FORMULIR PERMOHONAN INFORMASI DEBITUR (IDEB) PERSEORANGAN</h4>
+
+        <p>Yang bertanda tangan dibawah ini :</p>
 
         <table>
             <tr>
