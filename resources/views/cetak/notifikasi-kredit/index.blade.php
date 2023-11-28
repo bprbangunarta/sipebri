9b37fde4d95ffcbeb2e4a96e498468c2fc46619c
MARKOM-PC <dev@bprbangunarta.co.id>
2023-11-28 17:02:22 +0700

    Fix

--- resources/views/cetak/notifikasi-kredit/index.blade.php
+++ resources/views/cetak/notifikasi-kredit/index.blade.php
@@ -11,6 +11,19 @@
                         <div class="box-header with-border">
                             <i class="fa fa-file-text-o"></i>
                             <h3 class="box-title">NOTIFIKASI KREDIT</h3>
+
+                            <div class="box-tools">
+                                <form href="{{ route('notifikasi_kredit') }}" method="GET">
+                                    <div class="input-group input-group-sm hidden-xs" style="width: 170px;">
+                                        <input type="text" class="form-control pull-right" name="name" id="name"
+                                            value="{{ request('name') }}" placeholder="Search">
+
+                                        <div class="input-group-btn">
+                                            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                         <div class="box-body">
                             <table class="table table-bordered">
@@ -98,6 +111,11 @@
                                 </tbody>
                             </table>
                         </div>
+
+                        <div class="box-footer clearfix">
+                            {{ $data->withQueryString()->links('vendor.pagination.adminlte') }}
+                        </div>
+
                     </div>
                 </div>
             </div>
