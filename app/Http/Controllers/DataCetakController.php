b46d62e7aebbabc7497ccd11c672ea31c9668137
yandiyandhi <dev@bprbangunarta.co.id>
2023-11-29 07:36:33 +0700

    update

--- app/Http/Controllers/DataCetakController.php
+++ app/Http/Controllers/DataCetakController.php
@@ -691,13 +691,16 @@ class DataCetakController extends Controller
         $usul2 = "Kasi Analis";
         $usul3 = "Kabag Analis";
         $usul4 = "Direksi";
+
         for ($i = 0; $i < $count; $i++) {
             if ($data->isNotEmpty()) {
-                $data[$i]->kd_pengajuan = Crypt::encrypt($data[$i]->kode_pengajuan) ?? null;
-                $data[$i]->usulan1 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul1);
-                $data[$i]->usulan2 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul2);
-                $data[$i]->usulan3 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul3);
-                $data[$i]->usulan4 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul4);
+                foreach ($data as $item) {
+                    $item->kd_pengajuan = Crypt::encrypt($item->kode_pengajuan) ?? null;
+                    $item->usulan1 = Midle::data_usulan($item->kode_pengajuan, $usul1);
+                    $item->usulan2 = Midle::data_usulan($item->kode_pengajuan, $usul2);
+                    $item->usulan3 = Midle::data_usulan($item->kode_pengajuan, $usul3);
+                    $item->usulan4 = Midle::data_usulan($item->kode_pengajuan, $usul4);
+                }
             }
         }
 
@@ -705,22 +708,26 @@ class DataCetakController extends Controller
             $usul1 = "Customer Service";
             for ($i = 0; $i < $count; $i++) {
                 if ($data->isNotEmpty()) {
-                    $data[$i]->kd_pengajuan = Crypt::encrypt($data[$i]->kode_pengajuan) ?? null;
-                    $data[$i]->usulan1 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul1) ?? null;
-                    $data[$i]->usulan2 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul2) ?? null;
-                    $data[$i]->usulan3 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul3) ?? null;
-                    $data[$i]->usulan4 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul4) ?? null;
+                    foreach ($data as $item) {
+                        $item->kd_pengajuan = Crypt::encrypt($item->kode_pengajuan) ?? null;
+                        $item->usulan1 = Midle::data_usulan($item->kode_pengajuan, $usul1);
+                        $item->usulan2 = Midle::data_usulan($item->kode_pengajuan, $usul2);
+                        $item->usulan3 = Midle::data_usulan($item->kode_pengajuan, $usul3);
+                        $item->usulan4 = Midle::data_usulan($item->kode_pengajuan, $usul4);
+                    }
                 }
             }
         } elseif ($user->role_name == 'Kepala Kantor Kas') {
             $usul1 = "Kepala Kantor Kas";
             for ($i = 0; $i < $count; $i++) {
                 if ($data->isNotEmpty()) {
-                    $data[$i]->kd_pengajuan = Crypt::encrypt($data[$i]->kode_pengajuan) ?? null;
-                    $data[$i]->usulan1 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul1) ?? null;
-                    $data[$i]->usulan2 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul2) ?? null;
-                    $data[$i]->usulan3 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul3) ?? null;
-                    $data[$i]->usulan4 = Midle::data_usulan($data[$i]->kode_pengajuan, $usul4) ?? null;
+                    foreach ($data as $item) {
+                        $item->kd_pengajuan = Crypt::encrypt($item->kode_pengajuan) ?? null;
+                        $item->usulan1 = Midle::data_usulan($item->kode_pengajuan, $usul1);
+                        $item->usulan2 = Midle::data_usulan($item->kode_pengajuan, $usul2);
+                        $item->usulan3 = Midle::data_usulan($item->kode_pengajuan, $usul3);
+                        $item->usulan4 = Midle::data_usulan($item->kode_pengajuan, $usul4);
+                    }
                 }
             }
         }
