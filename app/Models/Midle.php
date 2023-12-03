fb12917c0b51e7777c9749086a71e52b3981c50f
yandiyandhi <dev@bprbangunarta.co.id>
2023-12-04 02:54:01 +0700

    update

--- app/Models/Midle.php
+++ app/Models/Midle.php
@@ -1330,7 +1330,7 @@ class Midle extends Model
 
         // URL dan QR Code dari Google Chart API
         // $url = 'https://sipebri.bprbangunarta.co.id/images?qrcode=';
-        $url = 'http://127.0.0.1:8000/verifikasi?qrcode=' . $data_url;
+        $url = 'http://sipebri.bprbangunarta.co.id/verifikasi?qrcode=' . $data_url;
         $uri = urlencode($url);
         $chartUrl = 'https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=' . $uri;
 
