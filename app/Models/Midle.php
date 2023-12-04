494a5fb3ad237a02917cea486b556542b085399a
MARKOM-PC <dev@bprbangunarta.co.id>
2023-12-04 13:47:29 +0700

    Fix

--- app/Models/Midle.php
+++ app/Models/Midle.php
@@ -685,7 +685,7 @@ class Midle extends Model
             ->leftJoin('users', 'data_survei.surveyor_kode', '=', 'users.code_user')
             ->where('data_pengajuan.tracking', '=', $role)
             ->orWhere(function ($query) {
-                $query->where('data_pengajuan.tracking', '=', 'Selesai')
+                $query->where('data_pengajuan.tracking', '=', 'Naik Komite I')
                     ->whereNull('data_notifikasi.pengajuan_kode');
             })
             ->where(function ($query) use ($name) {
@@ -732,11 +732,10 @@ class Midle extends Model
             ->leftJoin('data_kantor', 'data_survei.kantor_kode', '=', 'data_kantor.kode_kantor')
             ->leftJoin('users', 'data_survei.surveyor_kode', '=', 'users.code_user')
             ->where('data_pengajuan.tracking', '=', $role)
-            ->where('data_pengajuan.tracking', '=', 'Naik Komite II')
-            // ->orWhere(function ($query) {
-            //     $query->where('data_pengajuan.tracking', '=', 'Selesai')
-            //         ->where('data_notifikasi.pengajuan_kode', '=', null);
-            // })
+            ->orWhere(function ($query) {
+                $query->where('data_pengajuan.tracking', '=', 'Naik Komite II')
+                    ->whereNull('data_notifikasi.pengajuan_kode');
+            })
             ->where(function ($query) use ($name) {
                 $query->where('data_nasabah.nama_nasabah', 'like', '%' . $name . '%')
                     ->orWhere('data_survei.kantor_kode', 'like', '%' . $name . '%')
