4d2d3a9fdbe3c90a42050c72f94ebb29d015287d
Zulfadli Rizal <dev@bprbangunarta.co.id>
2023-12-14 05:34:42 +0700

    Fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,29 +12,58 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $survei = DB::table('data_pengajuan')
+        $pengajuan = DB::table('data_pengajuan')
             ->join('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
-            ->whereIn('data_pengajuan.tracking', [
-                'Penjadwalan',
-                'Proses Survei',
-                'Proses Analisa',
-                'Naik Kasi',
-                'Naik Komite I',
-                'Naik Komite II'
-            ])
-           ->count();
+            ->join('data_pendamping', 'data_pendamping.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
+            ->join('data_survei', 'data_survei.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
+            ->join('data_produk', 'data_produk.kode_produk', '=', 'data_pengajuan.produk_kode')
+            ->join('data_kantor', 'data_survei.kantor_kode', '=', 'data_kantor.kode_kantor')
+            ->join('v_users', 'v_users.code_user', '=', 'data_pengajuan.input_user')
+            ->count();
 
-        $disetujui = Pengajuan::where('status', 'Disetujui')
-            ->where('on_current', '1')
+        $sebelum_survey = DB::table('data_pengajuan')
+            ->join('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
+            ->join('data_survei', 'data_pengajuan.kode_pengajuan', '=', 'data_survei.pengajuan_kode')
+            ->join('data_kantor', 'data_survei.kantor_kode', '=', 'data_kantor.kode_kantor')
+            ->join('data_tracking', 'data_tracking.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
+            ->join('v_users', 'v_users.code_user', '=', 'data_survei.kasi_kode')
+            ->whereIn('data_pengajuan.tracking', ['Verifikasi Data', 'Penjadwalan', 'Proses Survey'])
+            ->count();
+
+        $sesudah_survey = DB::table('data_pengajuan')
+            ->join('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
+            ->join('data_survei', 'data_pengajuan.kode_pengajuan', '=', 'data_survei.pengajuan_kode')
+            ->join('data_kantor', 'data_survei.kantor_kode', '=', 'data_kantor.kode_kantor')
+            ->join('data_tracking', 'data_tracking.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
+            ->join('v_users', 'v_users.code_user', '=', 'data_survei.surveyor_kode')
+            ->whereIn('data_pengajuan.tracking', ['Proses Analisa', 'Persetujuan Komite', 'Naik Kasi', 'Naik Komite I', 'Naik Komite II'])
+            ->count();
+
+        $pencairan = DB::table('data_pengajuan')
+            ->join('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
+            ->join('data_survei', 'data_pengajuan.kode_pengajuan', '=', 'data_survei.pengajuan_kode')
+            ->join('data_kantor', 'data_survei.kantor_kode', '=', 'data_kantor.kode_kantor')
+            ->join('data_spk', 'data_spk.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
+            ->join('data_tracking', 'data_tracking.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
+            ->join('v_users', 'v_users.code_user', '=', 'data_pengajuan.input_user')
+            ->where('data_pengajuan.on_current', 1)
             ->count();
 
         $siap_realisasi = DB::table('data_pengajuan')
         ->join('data_nasabah', 'data_pengajuan.nasabah_kode', '=', 'data_nasabah.kode_nasabah')
-        ->join('data_notifikasi', 'data_notifikasi.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
+        ->join('data_pendamping', 'data_pendamping.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
         ->join('data_survei', 'data_survei.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
+        ->join('data_tracking', 'data_tracking.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
+        ->join('data_produk', 'data_produk.kode_produk', '=', 'data_pengajuan.produk_kode')
         ->join('data_kantor', 'data_survei.kantor_kode', '=', 'data_kantor.kode_kantor')
-        ->join('users', 'users.code_user', '=', 'data_survei.surveyor_kode')
-        ->where('data_pengajuan.on_current', '0')
+        ->join('v_users', 'v_users.code_user', '=', 'data_pengajuan.input_user')
+        ->join('data_notifikasi', 'data_notifikasi.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
+        ->leftJoin('data_spk', 'data_spk.pengajuan_kode', '=', 'data_pengajuan.kode_pengajuan')
+
+        ->where('data_pengajuan.on_current', 0)
+        ->where('data_pengajuan.status', 'Disetujui')
+        ->whereNotNull('data_notifikasi.keterangan')
+        ->whereNull('data_spk.no_spk')
         ->count();
 
         $penolakan = Pengajuan::where('status', 'Ditolak')
@@ -62,12 +91,12 @@ class DashboardController extends Controller
         }
         $total = array_sum($tak);
         $realisasi = $query->paginate(10);
-        $pengajuan = $survei + $siap_realisasi + $disetujui;
+        $survei = $sebelum_survey + $sesudah_survey;
 
         return view('dashboard', [
             'pengajuan' => $pengajuan,
             'survei' => $survei,
-            'disetujui' => $disetujui,
+            'disetujui' => $pencairan,
             'siap_realisasi' => $siap_realisasi,
             'penolakan' => $penolakan,
             'data' => $realisasi,
