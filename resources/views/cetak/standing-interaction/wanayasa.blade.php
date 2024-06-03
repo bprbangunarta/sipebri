c349d5b6a41aa5bfe7dd12d004faabb8fb98f756
yandiyandhi <dev@bprbangunarta.co.id>
2024-06-03 11:27:14 +0700

    update SI

--- resources/views/cetak/standing-interaction/wanayasa.blade.php
+++ resources/views/cetak/standing-interaction/wanayasa.blade.php
@@ -213,141 +213,71 @@
 
         <div class="page-break"></div>
 
-        <div class="header" style='font-family: "Calibri", sans-serif; margin-top: -30px;'>
+        <div class="header" style='font-family: "Calibri", sans-serif;'>
             <br>
             <br>
-            <br>
-            <h3><b><u>PERMOHONAN PEMBLOKIRAN <br> DAN <br> PENDEBETAN REKENING</u></b>
-            </h3>
+            <p>SURAT PERMOHONAN PEMBLOKIRAN DAN AUTODEBET</p>
         </div>
-
-        <p>Kepada :</p>
-        <p>
-            PT. Bank Mandiri (Persero) Tbk<br>
-            Cabang Purwakarta Wanayasa<br>
-            Kp. Krajan Rt. 06 Rw. 03 Ds. Wanayasa <br>
-            Purwakarta – 41174
-        </p>
-
-        <table class="info-table">
-            <tr>
-                <td colspan="2">Yang bertanda tangan di bawah ini :</td>
-            </tr>
-            <tr>
-                <td style="width: 25%;">Nama Lengkap</td>
-                <td style="width: 2%;">:</td>
-                <td>{{ $data->nama_nasabah }}</td>
-            </tr>
-            <tr>
-                <td class="nowrap">Tempat, Tanggal Lahir</td>
-                <td>:</td>
-                <td>{{ $data->tempat_lahir }}, {{ $data->tanggal_lahir }}</td>
-            </tr>
-            <tr>
-                <td>Nomor Rekening</td>
-                <td>:</td>
-                <td>{{ $data->no_rekening }}</td>
-            </tr>
-            <tr>
-                <td>Alamat</td>
-                <td>:</td>
-                <td style="text-align:justify;">
-                    {{ ucwords($data->alamat_ktp) }}
-                </td>
-            </tr>
-            <tr>
-                <td>Nomor KTP</td>
-                <td>:</td>
-                <td style="vertical-align: middle;">{{ $data->no_identitas }}</td>
-            </tr>
-            <tr>
-                <td>No. Telepon</td>
-                <td>:</td>
-                <td>{{ $data->no_telp }}</td>
-            </tr>
-        </table>
-
-        <p>Mohon agar dilakukan pemindahbukuan dana dari rekening saya tersebut di atas dengan penjelasan sebagai
-            berikut:</p>
-
-        <table class="info-table">
-            <tr>
-                <td style="width: 145px;">Waktu Pelaksanaan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                <td style="width: 2%;">:</td>
-                <td>Setiap tanggal 10 (Sepuluh)</td>
-            </tr>
-            <tr>
-                <td>Jangka Waktu</td>
-                <td>:</td>
-                <td>{{ $data->jangka_waktu }}</td>
-            </tr>
-            <tr>
-                <td>Jumlah</td>
-                <td>:</td>
-                <td>{{ 'Rp. ' . number_format($data->angsuran, '0', ',', '.') }}</td>
-            </tr>
-            <tr>
-                <td>Terbilang</td>
-                <td>:</td>
-                <td>
-                    <font class="text-hg" style="text-transform: capitalize;">
-                        {{ Riskihajar\Terbilang\Facades\Terbilang::make($data->angsuran) }}</font>
-                </td>
-            </tr>
-        </table>
 
         <br>
 
-        <table class="info-table">
-            <tr>
-                <td style="width: 145px;">Nomor Rekening</td>
-                <td style="width: 2%;">:</td>
-                <td>173-00-0808089-8</td>
-            </tr>
-            <tr>
-                <td>Atas Nama</td>
-                <td>:</td>
-                <td>Pamanukan Bangunarta</td>
-            </tr>
-            <tr>
-                <td>Bank Penerima</td>
-                <td>:</td>
-                <td>PT. Bank Mandiri (Persero) Tbk Cab. Purwakarta Wanayasa</td>
-            </tr>
-        </table>
+        <div class="page-dua" style="margin-top: 0px;">
+            <table class="info-table" style="line-height: 0.5cm;">
+                <tr>
+                    <td colspan="2">Yang bertanda tangan di bawah ini :</td>
+                </tr>
+                <tr>
+                    <td>Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    </td>
+                    <td>: {{ $data->nama_nasabah }}</td>
+                </tr>
+                <tr>
+                    <td>Alamat</td>
+                    <td>: {{ $data->alamat_ktp }}</td>
+                </tr>
+                <tr>
+                    <td>NIK</td>
+                    <td>: {{ $data->no_identitas }}</td>
+                </tr>
+            </table>
 
-        <p style="text-align: justify;">Biaya yang timbul dalam pelaksanaan standing instruction ini dapat dibebankan
-            kepada rekening saya di atas.
-            Demikian surat ini saya buat untuk dipergunakan sebagaimana mestinya dan akan berakhir sesuai dengan jangka
-            waktu atau setelah ada pemberitahuan tertulis dari saya.</p>
+            <p style="text-align: justify;">
+                Dengan ini memberi kuasa kepada PT. Bank Mandiri (Persero) Tbk Cab. Purwakarta Wanayasa untuk melakukan
+                pemblokiran dan autodebet rekening saya dengan nomor rek <b>{{ $data->no_rekening }}</b> atas nama
+                {{ $data->nama_nasabah }} ke no rekening <b>173-00-0808089-8</b> atas nama PT. BPR Bangunarta.
+            </p>
 
-        <table style="width:100%;">
-            <tr>
-                <td style="float: left;">
-                    <center>
-                        <p>Subang, {{ $data->tanggal_spk }}</p>
-                        <br>
-                        <p><i>Meterai 10.000</i></p>
-                        <br>
-                        <label
-                            style='margin-top: 10px; font-size:12px; display: block;'>{{ $data->nama_nasabah }}</label>
-                        <p style="margin-top: -9px;"> (..............................................)</p>
-                    </center>
-                </td>
-                <td style="float: right; width:38%; margin-bottom:0; margin-right:0;">
-                    <center>
-                        <p>Mengetahui & Menyetujui <br>
-                            PT. Bank Mandiri (Persero) Tbk
-                            Cab. Purwakarta Wanayasa
-                        </p>
-                        <br>
-                        <br>
+            <p>Adapun rinciannya sebagai berikut:</p>
+            <table class="info-table" style="margin-top: -10px; margin-left: 20px;">
+                <tr>
+                    <td>1.</td>
+                    <td>Pemblokiran sebesar&nbsp;&nbsp;&nbsp;</td>
+                    <td>: Rp.</td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>Autodebet sebesar</td>
+                    <td>: Rp.</td>
+                </tr>
+            </table>
+            <p style="text-align: justify;">Demikian surat kuasa ini saya ajukan untuk dapat ditindaklanjuti. Atas
+                perhatiannya saya ucapkan
+                terimakasih.</p>
 
-                        <p style="margin-top: 30px;">(..............................................)</p>
-                    </center>
-                </td>
-            </tr>
-        </table>
+            <table class="info-table" style="float: right;">
+                <tr>
+                    <td>
+                        <center>
+                            <p>Subang, {{ $data->tanggal_spk }}</p>
+                            <br>
+                            <br>
+                            <br>
+                            <p>{{ $data->nama_nasabah }}</p>
+                        </center>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
 
     <script>
