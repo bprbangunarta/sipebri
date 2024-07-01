bab5261b790391b6d685b1e98815f3ab3c0cc21c
yandiyandhi <dev@bprbangunarta.co.id>
2024-07-01 07:58:38 +0700

    update

--- app/Http/Controllers/DataCetakController.php
+++ app/Http/Controllers/DataCetakController.php
@@ -260,7 +260,7 @@ class DataCetakController extends Controller
 
                 //QRCode 
                 $qr = Midle::get_qrcode($enc, 'Notifikasi Disetujui', $cek->code_user_notif);
-                dd($cek);
+
                 return view('cetak-berkas.notifikasi-kredit.general', [
                     'data' => $cek,
                     'agunan' => $notifikasi_general,
