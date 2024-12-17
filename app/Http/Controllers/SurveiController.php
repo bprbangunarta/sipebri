15234703619c7590f7d5d721207261152640cfb5
yandiyandhi <dev@bprbangunarta.co.id>
2024-12-17 14:13:22 +0700

    update

--- app/Http/Controllers/SurveiController.php
+++ app/Http/Controllers/SurveiController.php
@@ -326,7 +326,7 @@ class SurveiController extends Controller
                 }
             }
 
-            $tgl = now()->addDay()->locale('id')->translatedFormat('d F Y');
+            $tgl = now()->locale('id')->translatedFormat('d F Y');
 
             // Mendapatkan Jumlah
             $surveyorCounts = (object) [];
