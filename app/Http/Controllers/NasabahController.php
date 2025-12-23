a17d985fc8e22c2df96e771bb27d11a439280632
Zulfadli Rizal <it@bprbangunarta.co.id>
2025-12-23 11:50:01 +0700

    fix kode_pengajuan

--- app/Http/Controllers/NasabahController.php
+++ app/Http/Controllers/NasabahController.php
@@ -144,25 +144,27 @@ class NasabahController extends Controller
             $ceknasabah['tanggal_lahir'] = $tanggal;
 
             //Generate kode otomatis dari kanan ke kiri data pengajuan
-            // $lasts = Pengajuan::latest('kode_pengajuan')->first();
-            // if (is_null($lasts)) {
-            //     $count = 339931;
-            // } else {
-            //     $count = (int) $lasts->kode_pengajuan + 1;
-            // }
-            // $lengths = 8;
-            // $kodes = str_pad($count, $lengths, '0', STR_PAD_LEFT);
+            $kodes = DB::transaction(function () {
+                $last = DB::table('data_pengajuan')
+                    ->lockForUpdate()
+                    ->selectRaw('MAX(CAST(kode_pengajuan AS UNSIGNED)) as max_kode')
+                    ->first();
 
-            $lasts = Pengajuan::orderByRaw('CAST(kode_pengajuan AS UNSIGNED) DESC')->first();
-            if (! $lasts) {
-                $count = 339931;
-            } else {
-                $count = (int) $lasts->kode_pengajuan + 1;
-            }
-            $kodes = str_pad($count, 8, '0', STR_PAD_LEFT);
+                $count = $last->max_kode
+                    ? $last->max_kode + 1
+                    : 339931;
+
+                $kode = str_pad($count, 8, '0', STR_PAD_LEFT);
+
+                DB::table('data_pengajuan')->insert([
+                    'kode_pengajuan' => $kode,
+                ]);
+
+                return $kode;
+            });
 
             $cekpengajuan['kode_pengajuan'] = $kodes;
-            $cekpengajuan['nasabah_kode'] = $ceknasabah['kode_nasabah'];
+            $cekpengajuan['nasabah_kode']   = $ceknasabah['kode_nasabah'];
 
             //Auth
             $usr = Auth::user()->code_user;
